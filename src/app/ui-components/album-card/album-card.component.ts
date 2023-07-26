ec4ef4049a1c1837d854863a1f00da552eeedca6
Juan Camacho <68751794+BalaclavaAM@users.noreply.github.com>
2023-07-25 22:57:19 -0500

    feature: Flip card!

--- src/app/ui-components/album-card/album-card.component.ts
+++ src/app/ui-components/album-card/album-card.component.ts
@@ -1,3 +1,4 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, Input, OnInit } from '@angular/core';
 import { SpotifyTopInfo } from 'src/app/models/spotifyTopInfo';
 
@@ -5,16 +6,34 @@ import { SpotifyTopInfo } from 'src/app/models/spotifyTopInfo';
   selector: 'app-album-card',
   templateUrl: './album-card.component.html',
   styleUrls: ['./album-card.component.sass'],
+  animations: [
+    trigger("cardFlip", [
+      state(
+        "default",
+        style({
+          transform: "none"
+        })
+      ),
+      state(
+        "flipped",
+        style({
+          transform: "rotateY(180deg)"
+        })
+      ),
+      transition("default => flipped", [animate("470ms")]),
+      transition("flipped => default", [animate("470ms")]),
+    ])]
 })
 export class AlbumCardComponent implements OnInit {
   displayData: boolean = false;
   smallTitle: string = "";
+  state: string = "default";
   @Input() albumData: SpotifyTopInfo | undefined = undefined;
 
   ngOnInit(){
     //displayData is equal to albumData.album 12 first characters
-    this.smallTitle = this.albumData?.album?.substring(0, 14) || "";
-    const originalLength = this.albumData?.album?.length || 0;
+    this.smallTitle = this.albumData?.album?.substring(0, 14) ?? "";
+    const originalLength = this.albumData?.album?.length ?? 0;
     if (this.smallTitle.length < originalLength)
     {
       this.smallTitle += "..";
@@ -24,5 +43,9 @@ export class AlbumCardComponent implements OnInit {
   clickDisplayData()
   {
     this.displayData = !this.displayData;
+  }
+
+  cardClicked(){
+    this.state = this.state === "default" ? "flipped" : "default";
   }
 }
