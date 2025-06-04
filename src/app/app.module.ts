8fa64e6e51e26be2eb22ea119b0855c1e2013029
Juan Ma <jcamacho@duppla.co>
2025-06-03 21:55:17 -0500

    More angular updates...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,52 +1,51 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { HomeComponent } from './pages/home/home.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavBarComponent } from './ui-components/nav-bar/nav-bar.component';
 import { AboutBotComponent } from './pages/about-bot/about-bot.component';
-import { OAuthModule } from 'angular-oauth2-oidc';
-import { CallbackComponent } from './pages/callback/callback.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AlbumCardComponent } from './ui-components/album-card/album-card.component';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { CallbackComponent } from './pages/callback/callback.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HomeComponent } from './pages/home/home.component';
+import { HttpClient, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { NavBarComponent } from './ui-components/nav-bar/nav-bar.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgModule, importProvidersFrom } from '@angular/core';
+import { OAuthModule } from 'angular-oauth2-oidc';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 
 export function createTranslateLoader(http: HttpClient){
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 }
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    NavBarComponent,
-    AboutBotComponent,
-    HomeComponent,
-    CallbackComponent,
-    AlbumCardComponent
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    AppRoutingModule,
-    HttpClientModule,
-    TranslateModule.forRoot({
-      loader: {
-        provide: TranslateLoader,
-        useFactory: (createTranslateLoader),
-        deps: [HttpClient]
-      }
-    }),
-    OAuthModule.forRoot(),
-    NgbModule,
-    ReactiveFormsModule,
-    FormsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+@NgModule({ declarations: [
+        AppComponent,
+        NavBarComponent,
+        AboutBotComponent,
+        HomeComponent,
+        CallbackComponent,
+        AlbumCardComponent
+    ],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        AppRoutingModule,
+        TranslateModule.forRoot({
+            loader: {
+                provide: TranslateLoader,
+                useFactory: createTranslateLoader,
+                deps: [HttpClient]
+            }
+        }),
+        OAuthModule.forRoot(),
+        NgbModule,
+        ReactiveFormsModule,
+        FormsModule
+    ],
+    providers: [
+        provideHttpClient(withInterceptorsFromDi())
+    ]
 })
 export class AppModule { }
